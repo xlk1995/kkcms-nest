38969809c04233f557f7572c692fa8aaeedd8a19
xlk <1148038404@qq.com>
2023-08-20 11:16:33 +0800

    feat: 使用mock数据

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,13 +1,14 @@
 // run command npx prisma db seed
 import { PrismaClient } from '@prisma/client'
+import { Random } from 'mockjs'
 const prisma = new PrismaClient()
 
 async function run() {
   for (let i = 0; i < 30; i++) {
     await prisma.user.create({
       data: {
-        name: 'abc',
-        password: '123',
+        name: Random.cname(),
+        password: Random.string(),
       },
     })
   }
